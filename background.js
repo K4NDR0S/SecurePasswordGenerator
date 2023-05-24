e8ff08504643f494b0ef573ff7318450baeb63db
JohanLiebert01 <96792492+JohanLiebert01@users.noreply.github.com>
2023-05-25 01:32:56 +0200

    v1- all works

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    // password starting value
     chrome.storage.sync.set({passwordLength: 12}, function() {
       console.log('Password\'s length set to 12');
     });
